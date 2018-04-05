88d67940720c062a89d5f3a94bfa560eb0e3fcad
Edwin Rodríguez <edwrodrig@gmail.com>
2018-04-05 11:37:18 -0300

    Changes

--- src/data/Image.php
+++ src/data/Image.php
@@ -16,28 +16,29 @@ class Image extends ImageItem
     public function __construct(string $file, string $suffix = '') {
         parent::__construct(__DIR__  . '/../../data', $file, $suffix);
         $this->output_folder = '/assets';
+        $this->set_salt();
     }
 
     public function __toString() : string {
         return (string)Site::cache()->update_cache($this);
     }
 
-    public static function image($file, int $svg_factor = 1) {
+    public static function image($file, int $size_hint = 1000) {
         $img = new Image($file);
-        $img->svg_factor = $svg_factor;
+        $img->set_size_hint($size_hint);
         return $img;
     }
 
-    public static function cover($file, $width, $height, int $svg_factor = 1) {
+    public static function cover($file, $width, $height, int $size_hint = 1000) {
         $img = new Image($file);
-        $img->svg_factor = $svg_factor;
+        $img->set_size_hint($size_hint);
         $img->set_cover($width, $height);
         return $img;
     }
 
-    public static function contain($file, $width, $height, $background_color = 'transparent', int $svg_factor = 1) {
+    public static function contain($file, $width, $height, $background_color = 'transparent', int $size_hint = 1000) {
         $img = new Image($file);
-        $img->svg_factor = $svg_factor;
+        $img->set_size_hint($size_hint);
         $img->set_contain($width, $height, $background_color);
         return $img;
     }
